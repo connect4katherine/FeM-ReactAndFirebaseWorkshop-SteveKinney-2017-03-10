8ffd55917647987fb0a7573e30f959b91a576867
Tamara Temple <tamouse@gmail.com>
2017-03-10 14:08:21 -0600

    2017-03-10T14:08:21

--- spirit-animal-look-book/src/firebase.js
+++ spirit-animal-look-book/src/firebase.js
@@ -1,11 +1,11 @@
 import firebase from 'firebase';
 
 const config = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  storageBucket: '',
-  messagingSenderId: ''
+    apiKey: "AIzaSyDp8FAxb3TiwUJj3cwY4kAuegNFJzqHIpQ",
+    authDomain: "spirit-animal-look-book-9d426.firebaseapp.com",
+    databaseURL: "https://spirit-animal-look-book-9d426.firebaseio.com",
+    storageBucket: "spirit-animal-look-book-9d426.appspot.com",
+    messagingSenderId: "1078087573977"
 };
 
 firebase.initializeApp(config);
